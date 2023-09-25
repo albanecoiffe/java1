fd478c27cbd0b56e1867bc295fa813d8cfa7fcff
albanecoiffe <90681785+albanecoiffe@users.noreply.github.com>
2023-09-25 15:52:27 -0400

    Delete septsemaine4/src directory

--- septsemaine4/src/Out.java
+++
@@ -1,8 +0,0 @@
-public class Out extends Exception{
-    public Out(){
-        this("your number is not between 1 and 10");
-    }
-    public Out(String message){
-        super(message);
-    }
-}
